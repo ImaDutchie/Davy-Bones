76935f54baf59829b0157def7187935eb8eef886
Tjeerd de Vries <tjeerd-d-vries@hotmail.nl>
2025-03-22 16:36:46 +0100

    oceaanscripts en nodes

---
+++ Scripts/._oceangeneration.gd
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c1e2b932b6006ef49c158cd37c149a9f1c4cf5ad7f9c98016c40e5c1f7985c70
+size 4096
--- Scripts/davy.gd
+++ Scripts/davy.gd
@@ -1,24 +1,3 @@
-extends CharacterBody2D
-
-var speed: int = 200
-var direction: Vector2 = Vector2(0,1)
-
-func _physics_process(delta: float) -> void:
-	var inputDir: Vector2 = Vector2(
-		Input.get_axis("ui_left", "ui_right"), 
-		Input.get_axis("ui_up", "ui_down")).normalized()
-	if inputDir.x > 0:
-		#Check if the player is moving right
-		direction = inputDir
-	elif inputDir.x < 0:
-		#Check if the player is moving left
-		direction = inputDir
-	elif inputDir.y > 0:
-		#Check if the player is moving up
-		direction = inputDir
-	elif inputDir.y < 0:
-		#Check if the player is moving down
-		direction = inputDir
-		
-	velocity = inputDir * speed
-	move_and_slide()
+version https://git-lfs.github.com/spec/v1
+oid sha256:8255caab24f8b31923da0b8955d7b6cc8266d22686c6886dfeec4fa04138051b
+size 636
