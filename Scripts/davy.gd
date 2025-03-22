15c1d02ce073c1f6696494a98e5dbb4f052ffe6a
Dominic van Eenbergen <dominicvaneenbergen@gmail.com>
2025-03-22 18:14:52 +0100

    Davy shooting

--- Scripts/davy.gd
+++ Scripts/davy.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9f115d85224d2e54b9a6eb899effefea19e6bb117d882a697e55be0657b72209
-size 752
+oid sha256:d60c2bb986bca0529d2b27a00bed6618b576ebb806b0532019f9982a1238aa47
+size 1057
